f11091dca055c28c43e2a06822eaa28cfe74bad3
atirson <fabiano.oliveira2213@gmail.com>
2022-09-09 16:50:18 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,7 @@
+import { AutheticantionParams } from 'domain/usecases/authentication'
+import { faker } from '@faker-js/faker'
+
+export const mockAuthentication = (): AutheticantionParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
