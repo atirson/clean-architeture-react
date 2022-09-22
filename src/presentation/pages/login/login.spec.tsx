810c129d494c8a95edef8b4cd457ce3bca2b619a
atirson <fabiano.oliveira2213@gmail.com>
2022-09-21 23:40:44 -0300

    test: ensure Login starts with inputs required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,9 +5,19 @@ import { Login } from '@/presentation/pages'
 describe('Login Component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
+
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('❌')
+
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('❌')
   })
 })
